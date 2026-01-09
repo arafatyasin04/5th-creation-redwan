bea6173e551b1893637ac8dd91a260210e160c4a
👀 ꪖ𝘳ꪖᠻꪖ𝓽  👻 <as710bdind@gmail.com>
2026-01-09 09:48:06 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -43,7 +43,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
       case "message_reaction":
         onReaction();
 
-                if(event.reaction == "🙂"){
+                if(event.reaction == "😒"){
   if(event.userID == "100083557261851"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
@@ -53,7 +53,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("")
   }
   }
-        if(event.reaction == "👍"){
+        if(event.reaction == "😠"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100083557261851"){
     message.unsend(event.messageID)
 }else{
